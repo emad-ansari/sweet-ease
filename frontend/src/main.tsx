ec2c0ea97c79fcd24d53309f4ae27829714221a2
92Mohammad <m.e89571828@gmail.com>
2025-09-22 09:29:49 +0530

    feat: created main.jsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { AuthProvider } from "./hooks/use-auth.tsx";
+import { SweetProvider } from "./hooks/use-sweet.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<AuthProvider>
+			<SweetProvider>
+				<App />
+			</SweetProvider>
+		</AuthProvider>
+	</StrictMode>
+);
